77bb96c077b2ed3ededc4aebfc7901318949be26
LucreziaCatapano <lucrezia@mrc-lmb.cam.ac.uk>
2025-02-20 18:02:24 +0000

    Update3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="tables",
+    name="aceDRG-tables",
     version="1",
     packages=find_packages(),
     install_requires=[],  # Add dependencies if needed
