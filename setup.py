2b41ce29eca3530804fe76168656b4ce6fb92573
LucreziaCatapano <lucrezia@mrc-lmb.cam.ac.uk>
2025-02-20 19:10:47 +0000

    Update 6

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name="aceDRG-tables",
     version="1",
     packages=find_packages(),
-    include_package_data=True,
-    install_requires=[],  # Add dependencies if needed
+    include_package_data=True,  # Ensures data files are included
+    package_data={
+        "tables": ["**/*"],  # Recursively include all files in the tables package
+    },
+    install_requires=[],
 )
